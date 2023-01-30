465ef8a29766879af1a4dfa4beeeaf7ab2f899b3
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-29 19:22:18 -0700

    stated fetch request

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const darkMode = document.getElementById('dark');
 const lights = Array.from(document.getElementsByClassName('light'));
-console.log(lights)
+
 darkMode.addEventListener('click', () => {
     lights.forEach((light) => {
         if(light.className === "light"){
             light.className = "dark";
-            console.log(light.className)
         } else{
             light.className = "light";
         }
     });
 })
 
-
+//need to  be able to grab the input from the search
+const test = document.querySelector('input').innerHTML;
+console.log(test);
+const submit = document.getElementById('submit');
+submit.addEventListener('cick', () => {
+   fetch(`url/${submit}`)
+   .then(res => res.json)
+   //.then(data => )
+})
