86a12e50ae29cb58f094ab2445fe94cdf5bdbae6
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-30 17:17:46 -0700

    hid API key and got a fetch request working

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Another Weather App
 
 Thank you for downloading another weather app. This app allows you to enter any city and get the weather for it thanks to the OpenWeather API. It will also show you the chances of rain of the searched area.
+
+
+NOTE: The OpenWeather API requires a key and there is a hidden file which is what allows the GET request to work.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {API_KEY} from "../config.js";
+
 const darkMode = document.getElementById('dark');
 const lights = Array.from(document.getElementsByClassName('light'));
 
@@ -10,14 +12,17 @@ darkMode.addEventListener('click', () => {
         }
     });
 })
-
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=${API_KEY}`)
+   .then(res => res.json())
+   .then(data => console.log(data))
 //need to  be able to grab the input from the search
-const test = document.querySelector('input').innerHTML;
+const test = document.querySelector('input').value;
 console.log(test);
 const submit = document.getElementById('submit');
-submit.addEventListener('cick', () => {
-   console.log(submit)
-    fetch(`url/${submit}`)
-   .then(res => res.json)
-   //.then(data => )
+submit.addEventListener('click', (e) => {
+   console.log(e.target.value)
+   console.log(test)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=${API_KEY}`)
+   .then(res => res.json())
+   .then(data => console.log(data))
 })
